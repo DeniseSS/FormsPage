e4a4e81ed8b831d048c519d0aa59283c7103e0e4
DeniseSS <https://github.com/DeniseSS>
2022-10-27 22:45:38 -0300

    included validators and table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import {MatGridListModule} from '@angular/material/grid-list';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatTableModule} from '@angular/material/table';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -23,6 +24,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
   ],
   imports: [
+    MatTableModule,
     FlexLayoutModule,
     MatIconModule,
     MatToolbarModule,
